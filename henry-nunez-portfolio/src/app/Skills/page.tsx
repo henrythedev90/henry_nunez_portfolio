cbb897c52b0cd7ff79e7e2368a4dcea24f296e39
henrythedev90 <henrythedev90@gmail.com>
2024-09-23 05:23:40 -0400

    added animations to page

--- henry-nunez-portfolio/src/app/Skills/page.tsx
+++ henry-nunez-portfolio/src/app/Skills/page.tsx
@@ -1,3 +1,20 @@
+import { SKILLS_TYPE } from "../_lib/skillsType";
+import "./styles.css";
 export default function Skills() {
-  return <div>this is my skills page</div>;
+  return (
+    <div className="skills_container">
+      <div className="skills_atom">
+        {SKILLS_TYPE.map((item, index) => {
+          return (
+            <div className="skills_orbit" key={index}>
+              <div className="skills_path">
+                <div className="skills_electron">{item.type}</div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="skills_nucleus">Skills</div>
+      </div>
+    </div>
+  );
 }
---
+++ henry-nunez-portfolio/src/app/_lib/skillsType.tsx
@@ -0,0 +1,6 @@
+export const SKILLS_TYPE: { path: string; type: string }[] = [
+  { path: "/Front-end", type: "Front-End" },
+  { path: "/Back-end", type: "Back-End" },
+  { path: "/Tools", type: "Tools" },
+  { path: "/Testing", type: "Testing" },
+];
