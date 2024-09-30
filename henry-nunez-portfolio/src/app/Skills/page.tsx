31a49f35384bd6a8c488eda32f6b03cc3173e136
henrythedev90 <henrythedev90@gmail.com>
2024-09-30 18:48:32 -0400

    got the text to appear at a 180 degree

--- henry-nunez-portfolio/src/app/Skills/page.tsx
+++ henry-nunez-portfolio/src/app/Skills/page.tsx
@@ -1,6 +1,10 @@
 import { SKILLS_TYPE } from "../_lib/skillsType";
 import "./styles.css";
 export default function Skills() {
+  const rotateText = {
+    display: "inline-block",
+    transform: "rotateX(180deg)",
+  };
   return (
     <div className="skills_container">
       <div className="skills_atom">
@@ -8,7 +12,9 @@ export default function Skills() {
           return (
             <div className="skills_orbit" key={index}>
               <div className="skills_path">
-                <div className="skills_electron">{item.type}</div>
+                <div className="skills_electron">
+                  <span style={rotateText}>{item.type}</span>
+                </div>
               </div>
             </div>
           );
