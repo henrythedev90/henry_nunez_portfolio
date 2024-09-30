6ef03cefd78ea7b4023f1056e53dd824dbd98ae8
henrythedev90 <henrythedev90@gmail.com>
2024-09-30 19:40:48 -0400

    orbit is now responsive

--- henry-nunez-portfolio/src/app/Skills/page.tsx
+++ henry-nunez-portfolio/src/app/Skills/page.tsx
@@ -6,20 +6,24 @@ export default function Skills() {
     transform: "rotateX(180deg)",
   };
   return (
-    <div className="skills_container">
-      <div className="skills_atom">
-        {SKILLS_TYPE.map((item, index) => {
-          return (
-            <div className="skills_orbit" key={index}>
-              <div className="skills_path">
-                <div className="skills_electron">
-                  <span style={rotateText}>{item.type}</span>
+    <div>
+      <div className="skills_container">
+        <div className="skills_atom">
+          {SKILLS_TYPE.map((item, index) => {
+            return (
+              <div className="skills_orbit" key={index}>
+                <div className="skills_path">
+                  <div className="skills_electron">
+                    <span style={rotateText}>{item.type}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-        <div className="skills_nucleus">Skills</div>
+            );
+          })}
+          <div className="skills_nucleus">
+            <span>Skills</span>
+          </div>
+        </div>
       </div>
     </div>
   );
