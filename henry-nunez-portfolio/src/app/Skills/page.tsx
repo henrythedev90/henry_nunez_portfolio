ac30bb4b20adec052cefc739a3f8337f3fd68d3a
henrythedev90 <henrythedev90@gmail.com>
2024-10-01 16:06:32 -0400

    setting up the div for skill types

--- henry-nunez-portfolio/src/app/Skills/page.tsx
+++ henry-nunez-portfolio/src/app/Skills/page.tsx
@@ -1,4 +1,5 @@
 import { SKILLS_TYPE } from "../_lib/skillsType";
+import { SKILLS } from "../_lib/skills";
 import "./styles.css";
 export default function Skills() {
   const rotateText = {
@@ -6,7 +7,7 @@ export default function Skills() {
     transform: "rotateX(180deg)",
   };
   return (
-    <div>
+    <div className="skills_page">
       <div className="skills_container">
         <div className="skills_atom">
           {SKILLS_TYPE.map((item, index) => {
@@ -26,6 +27,48 @@ export default function Skills() {
         </div>
       </div>
       <div className="skills_list"></div>
+      <div className="skills_list_mobile">
+        <div className="skills_front_end">
+          <h1>{SKILLS_TYPE[0].type}</h1>
+          <div>
+            {SKILLS.filter((item) => item.type === "Front-End").map(
+              (item, index) => {
+                return <p key={index}>{item.title}</p>;
+              }
+            )}
+          </div>
+        </div>
+        <div className="skills_back_end">
+          <h1>{SKILLS_TYPE[1].type}</h1>
+          <div>
+            {SKILLS.filter((item) => item.type === "Back-End").map(
+              (item, index) => {
+                return <p key={index}>{item.title}</p>;
+              }
+            )}
+          </div>
+        </div>
+        <div className="skills_tools">
+          <h1>{SKILLS_TYPE[2].type}</h1>
+          <div>
+            {SKILLS.filter((item) => item.type === "Tools").map(
+              (item, index) => {
+                return <p key={index}>{item.title}</p>;
+              }
+            )}
+          </div>
+        </div>
+        <div className="skills_testing">
+          <h1>{SKILLS_TYPE[3].type}</h1>
+          <div>
+            {SKILLS.filter((item) => item.type === "Testing").map(
+              (item, index) => {
+                return <p key={index}>{item.title}</p>;
+              }
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
