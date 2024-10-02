336bbfca9accea5bb13a412bd19ef1430d69939a
henrythedev90 <henrythedev90@gmail.com>
2024-10-02 18:57:40 -0400

    about to start toggle

--- henry-nunez-portfolio/src/app/Skills/page.tsx
+++ henry-nunez-portfolio/src/app/Skills/page.tsx
@@ -36,57 +36,49 @@ export default function Skills() {
           <div className="toggle_skills" onClick={() => setIsOpen(!isOpen)}>
             <h1 className="skills_list_mobile_title">{SKILLS_TYPE[0].type}</h1>
           </div>
-          <div className="skills_list_mobile_content">
-            <ul className="skills_list_mobile_item">
-              {SKILLS.filter((item) => item.type === "Front-End").map(
-                (item, index) => {
-                  return <li key={index}>{item.title}</li>;
-                }
-              )}
-            </ul>
-          </div>
+          <ul className="skills_list_mobile_item">
+            {SKILLS.filter((item) => item.type === "Front-End").map(
+              (item, index) => {
+                return <li key={index}>{item.title}</li>;
+              }
+            )}
+          </ul>
         </div>
         <div className="skills_back_end">
           <div className="toggle_skills" onClick={() => setIsOpen(!isOpen)}>
             <h1 className="skills_list_mobile_title">{SKILLS_TYPE[1].type}</h1>
           </div>
-          <div className="skills_list_mobile_content">
-            <ul className="skills_list_mobile_item">
-              {SKILLS.filter((item) => item.type === "Back-End").map(
-                (item, index) => {
-                  return <li key={index}>{item.title}</li>;
-                }
-              )}
-            </ul>
-          </div>
+          <ul className="skills_list_mobile_item">
+            {SKILLS.filter((item) => item.type === "Back-End").map(
+              (item, index) => {
+                return <li key={index}>{item.title}</li>;
+              }
+            )}
+          </ul>
         </div>
         <div className="skills_tools">
           <div className="toggle_skills" onClick={() => setIsOpen(!isOpen)}>
             <h1 className="skills_list_mobile_title">{SKILLS_TYPE[2].type}</h1>
           </div>
-          <div className="skills_list_mobile_content">
-            <ul className="skills_list_mobile_item">
-              {SKILLS.filter((item) => item.type === "Tools").map(
-                (item, index) => {
-                  return <li key={index}>{item.title}</li>;
-                }
-              )}
-            </ul>
-          </div>
+          <ul className="skills_list_mobile_item">
+            {SKILLS.filter((item) => item.type === "Tools").map(
+              (item, index) => {
+                return <li key={index}>{item.title}</li>;
+              }
+            )}
+          </ul>
         </div>
         <div className="skills_testing">
           <div className="toggle_skills" onClick={() => setIsOpen(!isOpen)}>
             <h1 className="skills_list_mobile_title">{SKILLS_TYPE[3].type}</h1>
           </div>
-          <div className="skills_list_mobile_content">
-            <ul className="skills_list_mobile_item">
-              {SKILLS.filter((item) => item.type === "Testing").map(
-                (item, index) => {
-                  return <li key={index}>{item.title}</li>;
-                }
-              )}
-            </ul>
-          </div>
+          <ul className="skills_list_mobile_item">
+            {SKILLS.filter((item) => item.type === "Testing").map(
+              (item, index) => {
+                return <li key={index}>{item.title}</li>;
+              }
+            )}
+          </ul>
         </div>
       </div>
     </div>
