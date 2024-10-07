b28cda718710a00ca56a9a0336290dd7cf6a9f16
henrythedev90 <henrythedev90@gmail.com>
2024-10-07 18:35:31 -0400

    added my profile image

--- henry-nunez-portfolio/src/app/Home/page.tsx
+++ henry-nunez-portfolio/src/app/Home/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./styles.css";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div className="home_page">
-      <div>
-        <div className="card-container">
-          <div className="card white">hello</div>
-          <div className="card black"></div>
-          <div className="card white"></div>
+      <div className="first_row first_column">
+        <div className="card-container image_container">
+          <div className="card white image_card">
+            <Image
+              src="/profile.jpg"
+              alt="profile"
+              width={200} // Increased width for better clarity
+              height={200} // Increased height for better clarity
+              layout="responsive" // Makes the image responsive
+              sizes="(max-width: 600px) 100vw"
+              priority
+              className="profile_image"
+            />
+          </div>
           <div className="card black"></div>
           <div className="card white"></div>
           <div className="card black"></div>
