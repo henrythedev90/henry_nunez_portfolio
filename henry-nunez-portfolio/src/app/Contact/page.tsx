27d6fb308a4e37cf2ca8d7e625b51f58e6d24f0a
henrythedev90 <henrythedev90@gmail.com>
2024-10-14 17:28:30 -0400

    working on the contact page

--- henry-nunez-portfolio/src/app/Contact/page.tsx
+++ henry-nunez-portfolio/src/app/Contact/page.tsx
@@ -4,29 +4,30 @@
 // // import "./styles.css";
 
 export default function Contact() {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const openModal = () => {
-  //   setIsModalOpen(true);
-  // };
-  // const closeModal = () => {
-  //   setIsModalOpen(false);
-  // };
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   closeModal();
-  //   alert("Form submitted successfully!");
-  // };
-  // return (
-  //   <div>
-  //     <button onClick={openModal}>Open Modal</button>
-  //     <Modal isOpen={isModalOpen} isClose={closeModal}>
-  //       <form onSubmit={handleSubmit}>
-  //         <input type="text" placeholder="Name" required />
-  //         <input type="email" placeholder="Email" required />
-  //         <button type="submit">Submit</button>
-  //       </form>
-  //     </Modal>
-  //   </div>
-  // );
-  return <div>Contact</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Let's get in touch!</h2>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name:
+          <input type="text" placeholder="Name" required />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input type="email" placeholder="Email" required />
+        </label>
+        <label htmlFor="message">
+          Message:
+          <textarea placeholder="Message" required />
+        </label>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
--- henry-nunez-portfolio/src/app/page.tsx
+++ henry-nunez-portfolio/src/app/page.tsx
@@ -1,15 +1,30 @@
+"use client";
 import React from "react";
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import Home from "./Home/page";
 import Skills from "./Skills/page";
 import Contact from "./Contact/page";
 
 const Page = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 601);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Fragment>
       <Home />
       <Skills />
-      <Contact />
+      {isMobile && <Contact />}
     </Fragment>
   );
 };
