2da01b1b82c8055825cb8f8133c80e20cbb52d19
henrythedev90 <henrythedev90@gmail.com>
2024-10-15 16:10:37 -0400

    got sendgrid to work

--- henry-nunez-portfolio/src/app/Contact/page.tsx
+++ henry-nunez-portfolio/src/app/Contact/page.tsx
@@ -1,10 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function Contact() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    fetch("/api/mail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    setValues({ name: "", email: "", message: "" });
   };
 
   return (
@@ -14,15 +34,35 @@ export default function Contact() {
         <form className="contact-form" onSubmit={handleSubmit}>
           <label htmlFor="name">
             <span>Name:</span>
-            <input type="text" placeholder="Name" required />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={handleChange}
+              value={values.name}
+              required
+            />
           </label>
           <label htmlFor="email">
             <span>Email:</span>
-            <input type="email" placeholder="Email" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              value={values.email}
+              required
+            />
           </label>
           <label htmlFor="message">
             <span>Message:</span>
-            <textarea placeholder="Message" required />
+            <textarea
+              name="message"
+              placeholder="Message"
+              onChange={handleChange}
+              value={values.message}
+              required
+            />
           </label>
           <button className="submit-button" type="submit">
             Submit
