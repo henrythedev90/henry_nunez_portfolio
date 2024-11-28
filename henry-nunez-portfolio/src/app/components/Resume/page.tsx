37bb50ce97b5deb5d90b92656df4853ad20b8a51
henrythedev90 <henrythedev90@gmail.com>
2024-11-28 17:45:41 -0500

    added the pdf file on desktop

--- henry-nunez-portfolio/src/app/components/Resume/page.tsx
+++ henry-nunez-portfolio/src/app/components/Resume/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./styles.css";
 
 const PdfViewer = () => {
-  return <div>hello world, this is where your resume belongs</div>;
+  return (
+    <div className="pdf_container">
+      <h1>Resume</h1>
+      <div className="pdf_file_container">
+        <embed
+          src="/pdfs/Henry_Nunez_Resume_2024.pdf"
+          width="1126px"
+          height="1150px"
+          type="application/pdf"
+          style={{ border: "none" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PdfViewer;
