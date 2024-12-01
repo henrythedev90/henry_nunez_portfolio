daeb8cb75ff82cb4b62092daeef1cbf4d2b3c564
henrythedev90 <henrythedev90@gmail.com>
2024-12-01 16:51:00 -0500

    working on fixing some of the errors

--- henry-nunez-portfolio/.next/server/webpack-runtime.js
+++ henry-nunez-portfolio/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("2aa0ee7fc5715e8a")
+/******/ 		__webpack_require__.h = () => ("614f33750664b8a3")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- henry-nunez-portfolio/.next/static/chunks/webpack.js
+++ henry-nunez-portfolio/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c3d70c3c14a4954"; }
+/******/ 		__webpack_require__.h = function() { return "d034e5604121c0b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
