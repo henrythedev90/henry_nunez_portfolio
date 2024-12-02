9170ba816031eaee109de1e2c964c7bf0365b843
henrythedev90 <henrythedev90@gmail.com>
2024-12-01 19:15:37 -0500

    cleaning

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const { webpack } = require("next/dist/compiled/webpack/webpack");
-
-module.exports = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
-};
